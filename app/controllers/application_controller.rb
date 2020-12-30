49616e09b046cedfda5427738464fb6684731e21
saki-smile <sa.su.ko0419@gmail.com>
2020-12-30 09:00:55 +0000

    [update]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "¡Hola, mundo!"
   end
-  
+
   def goodbye
-    render html: "goodbye, world!"
+    render html: "goodbye, world2"
   end
 end
